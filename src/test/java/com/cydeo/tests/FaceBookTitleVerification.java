bf79cf7909c5a08ce2a0bcaa1bd983edfe80284f
AkbermetM <Bemaibragimovna@gmail.com>
2023-01-25 10:58:42 -0600

    first commit

---
+++ src/test/java/com/cydeo/tests/FaceBookTitleVerification.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FaceBookTitleVerification {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+    }
+}
